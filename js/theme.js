0f9530158d0ac10bad0d238b5b99468c142380dd
Anikesh02 <anikeshkulalinft-@atharvacoe.ac.in>
2024-10-22 12:17:13 +0530

    Second Commit

--- js/theme.js
+++ js/theme.js
@@ -1,7 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
   const input = document.querySelector('#themeInput')
-  const switchTheme = ({ target: { checked } }) =>
-    document.documentElement.setAttribute('data-theme', checked ? 'dark' : 'light')
-
-  input.addEventListener('change', switchTheme)
+  
 })
